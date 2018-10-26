0811df1335b571a8f07d627c0f7779a8cd1e3ced
Reginald <reginald@live.at>
2018-10-26 22:26:08 +0100

    Added untested method

--- ProductMethods.cs
+++ ProductMethods.cs
@@ -101,26 +101,34 @@ namespace NewIn
             }
         }
 
-        public static void ShowProduct(List<Product> someList) // Get current inventory stored in db
+        public static List<Product> ShowProducts(List<Product> someList) // Not tried, yet
         {
+            var tempList = new List<Product>();
             using( var con = new SqliteConnection("DataSource = InventoryDB.db"))
             {
                 con.Open();
                 
-                string query = "INSERT INTO Inventory ( Name, Brand, Price, Quantity) VALUES ( @Name, @Brand, @Price, @Quantity)";
+                string query = "SELECT Name, Brand, Price, Quantity FROM Inventory";
 
-                foreach(var product in someList){
-                    using(var cmd = new SqliteCommand(query, con)){
-                        //cmd.Parameters.AddWithValue("@StudentID", student.StudentID);
-                        cmd.Parameters.AddWithValue("@Name", product.productName);
-                        cmd.Parameters.AddWithValue("@Brand", product.brand);
-                        cmd.Parameters.AddWithValue("@Price", product.productPrice);
-                        cmd.Parameters.AddWithValue("@Quantity", product.productQuantity);
-                        cmd.ExecuteNonQuery();
-                    } // using cmd
-                } // foreach
+                    using(var cmd = new SqliteCommand(query, con))
+                    {
+                        using(var reader = cmd.ExecuteReader())
+                        {
+                            while(reader.Read())
+                            {
+                                var tempProduct = new Product();
+                                tempProduct.productName     = Convert.ToString(reader["Name"]);
+                                tempProduct.brand           = Convert.ToString(reader["Brand"]);
+                                tempProduct.productPrice    = Convert.ToDecimal(reader["Price"]);
+                                tempProduct.productQuantity = Convert.ToInt32(reader["Quantity"]);
+                                tempList.Add(tempProduct);
+                            } 
+                        } // reader
+                    }
+                
                 con.Close();
-            }
+            } // connection
+            return tempList.ToList();
         }
 
         public static void AddedInventoryValue(List<Product> someList)
